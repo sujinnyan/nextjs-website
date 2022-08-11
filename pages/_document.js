61a4cbfe780cd1ae5868c6beb10ad376f531fd3a
sujinnyan <ansujinny@gmail.com>
2022-08-11 15:09:43 -0400

    edited file

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,6 @@ export default function Document() {
         <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" cross0rigin/>
         <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,600&display=swap" rel="stylesheet"/>
         </Head>
       
